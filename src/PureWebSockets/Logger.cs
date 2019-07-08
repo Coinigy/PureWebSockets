055f26eb36a4e2d9d83daab4679e942c8aa6f801
ByronAP <byronap@coinigy.com>
2019-07-08 10:44:39 -0400

    create Logger class and make async methods

---
+++ src/PureWebSockets/Logger.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace PureWebSockets
+{
+    internal class Logger
+    {
+        private readonly PureWebSocketOptions _options;
+
+        public Logger(PureWebSocketOptions options)
+        {
+            _options = options;
+        }
+
+        internal void Log(string message, [CallerMemberName] string memberName = "")
+        {
+            if (_options.DebugMode)
+            {
+                Task.Run(() => _options.DebugOutput.WriteLine($"{DateTime.Now:O} PureWebSocket.{memberName}: {message}"));
+            }
+        }
+
+        internal Task LogAsync(string message, [CallerMemberName] string memberName = "")
+        {
+            if (_options.DebugMode)
+            {
+                return _options.DebugOutput.WriteLineAsync($"{DateTime.Now:O} PureWebSocket.{memberName}: {message}");
+            }
+
+            return Task.CompletedTask;
+        }
+
+        internal void LogData(string message, byte[] data, [CallerMemberName] string memberName = "")
+        {
+            if (_options.DebugMode)
+            {
+                Task.Run(() =>
+                    _options.DebugOutput.WriteLine(
+                        $"{DateTime.Now:O} PureWebSocket.{memberName}: {message}, data: {BitConverter.ToString(data)}"));
+            }
+        }
+
+        internal Task LogDataAsync(string message, byte[] data, [CallerMemberName] string memberName = "")
+        {
+            if (_options.DebugMode)
+            {
+                return _options.DebugOutput.WriteLineAsync(
+                    $"{DateTime.Now:O} PureWebSocket.{memberName}: {message}, data: {BitConverter.ToString(data)}");
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
